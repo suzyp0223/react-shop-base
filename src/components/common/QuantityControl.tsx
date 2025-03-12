fefed57bea24d9947b5e8ae5952bbbb904b5def0
suzy <suzyp0223@gmail.com>
2025-03-12 17:17:25 +0900

    주석제거2

--- src/components/common/QuantityControl.tsx
+++ src/components/common/QuantityControl.tsx
@@ -11,17 +11,13 @@ const QuantityControl: React.FC<QuantityControlProps> = ({ productId, price, qua
   return (
     <div className="textBtnWrap">
       <div className="btnBox">
-        {/* ✅ 개별 상품 총 가격 표시 (수량 × 가격) */}
         <p className="textTotalPrice">총 가격: ${Math.round(price * quantity)}</p>
-        {/* ✅ 개별 상품 수량 감소 */}
         <button className="btn btn-primary" onClick={() => updateQuantity(productId, -1)}>
           -
         </button>
 
-        {/* ✅ 개별 상품 수량 표시 */}
         <button className="btn btn-num">{quantity}</button>
 
-        {/* ✅ 개별 상품 수량 증가 */}
         <button className="btn btn-primary" onClick={() => updateQuantity(productId, 1)}>
           +
         </button>
